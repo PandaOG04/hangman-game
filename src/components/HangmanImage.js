bc6415ebdb386e41b9a82064365658950b6e7eff
PandaOG04 <erenkhan2006@gmail.com>
2025-06-12 11:22:48 +0300

    Initial commit - Hangman game

--- src/components/HangmanImage.js
+++ src/components/HangmanImage.js
@@ -1,20 +1,17 @@
-import React from "react";
-import step0 from "../assets/images/step0.png";
-import step1 from "../assets/images/step1.png";
-import step2 from "../assets/images/step2.png";
-import step3 from "../assets/images/step3.png";
-import step4 from "../assets/images/step4.png";
-import step5 from "../assets/images/step5.png";
-import step6 from "../assets/images/step6.png";
+import step0 from '../assets/images/step0.png';
+import step1 from '../assets/images/step1.png';
+import step2 from '../assets/images/step2.png';
+import step3 from '../assets/images/step3.png';
+import step4 from '../assets/images/step4.png';
+import step5 from '../assets/images/step5.png';
+import step6 from '../assets/images/step6.png';
 
-const steps = [step0, step1, step2, step3, step4, step5, step6];
+const images = [step0, step1, step2, step3, step4, step5, step6];
 
-const HangmanImage = ({ wrongGuesses }) => {
+export default function HangmanImage({ wrongGuesses }) {
   return (
     <div className="hangman-img">
-      <img src={steps[wrongGuesses]} alt={`Hangman step ${wrongGuesses}`} />
+      <img src={images[wrongGuesses]} alt={`Hangman step ${wrongGuesses}`} />
     </div>
   );
-};
-
-export default HangmanImage;
+}
--- src/components/HelpModal.js
+++ src/components/HelpModal.js
@@ -1,14 +1,18 @@
-import React from "react";
+import React from 'react';
 
-const HelpModal = ({ show, onClose }) => {
-  if (!show) return null;
+function HelpModal({ onClose }) {
   return (
     <div className="modal">
       <div className="modal-content">
-        <h2>How to Play Hangman</h2>
-        <p>Guess the hidden word one letter at a time. Each wrong guess draws part of the hangman. You lose after 6 wrong guesses. Click Restart to play again.</p>
+        <h2>How to Play</h2>
+        <p>
+          Guess the hidden word by clicking letters. Each incorrect guess adds a part to the hangman.
+          You lose after 6 wrong guesses. Click "Restart" to play again!
+        </p>
         <button onClick={onClose}>Close</button>
       </div>
     </div>
   );
-};
+}
+
+export default HelpModal;
