c0892ac1781f96e223045dd5924ab29cfce96bfa
PandaOG04 <103189572+PandaOG04@users.noreply.github.com>
2025-06-13 10:21:07 +0300

    Update HangmanImage.js
    Fixing directory of images.

--- src/components/HangmanImage.js
+++ src/components/HangmanImage.js
@@ -1,10 +1,10 @@
-import step0 from '../assets/images/step0.png';
-import step1 from '../assets/images/step1.png';
-import step2 from '../assets/images/step2.png';
-import step3 from '../assets/images/step3.png';
-import step4 from '../assets/images/step4.png';
-import step5 from '../assets/images/step5.png';
-import step6 from '../assets/images/step6.png';
+import step0 from '../assets/step0.png';
+import step1 from '../assets/step1.png';
+import step2 from '../assets/step2.png';
+import step3 from '../assets/step3.png';
+import step4 from '../assets/step4.png';
+import step5 from '../assets/step5.png';
+import step6 from '../assets/step6.png';
 
 const images = [step0, step1, step2, step3, step4, step5, step6];
 
